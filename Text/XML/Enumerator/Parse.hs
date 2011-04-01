3afadb746da42328f2df1f59561d525a18b96c26
Aristid Breitkreuz <aristidb@googlemail.com>
2011-04-01 05:05:08 +0200

    fix dangerous guard order in processNested (this function is scary)

--- Text/XML/Enumerator/Parse.hs
+++ Text/XML/Enumerator/Parse.hs
@@ -844,8 +844,8 @@ processNested isElem k0 = E.continue (loop (Just []) k0)
                             case x of
                                 (EventBeginElement n _) -> go (Just $ n:ns) (ts ++ [x]) xs
                                 (EventEndElement n)
-                                    | isElem && null (tail ns) -> (Just [], ts ++ [x], xs)
                                     | isElem && null ns -> (Nothing, ts, xxs)
+                                    | isElem && null (tail ns) -> (Just [], ts ++ [x], xs)
                                     | not isElem && null ns -> (Just [], ts, xxs)
                                     -- | null (if isElem then tail ns else ns) -> (Just [], if isElem then ts ++ [x] else ts, (f xxs))
                                     | n == head ns -> go (Just $ tail ns) (ts ++ [x]) xs
