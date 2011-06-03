49efdddea6d4c6a0545650d08d007228bb9ecc81
Aristid Breitkreuz <aristidb@googlemail.com>
2011-06-03 12:53:16 +0200

    Cursor: toCursor -> fromNode

--- runtests.hs
+++ runtests.hs
@@ -171,7 +171,7 @@ name (c:cs) = ($ name cs) $ case Cu.node c of
                               _ -> id
 
 cursor =
-    Cu.toCursor $ NodeElement e
+    Cu.fromNode $ NodeElement e
   where
     Document _ e _ = D.parseLBS_ input decodeEntities
     input = L.concat
