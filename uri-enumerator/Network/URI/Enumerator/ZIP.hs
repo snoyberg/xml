45166d261606cd0706009d3bae59da8627953ce9
Michael Snoyman <michael@snoyman.com>
2011-09-08 13:22:40 +0300

    uri ZIP support

---
+++ uri-enumerator/Network/URI/Enumerator/ZIP.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module Network.URI.Enumerator.ZIP
+    ( zipScheme
+    , ZipException (..)
+    , toArchive
+    , readZipFile
+    ) where
+
+import Network.URI.Enumerator
+import Codec.Archive.Zip
+import Data.Text (Text, snoc, unpack)
+import qualified Data.Set as Set
+import Control.Exception (Exception)
+import Data.Typeable (Typeable)
+import Data.Enumerator (throwError, enumList)
+import qualified Data.ByteString.Lazy as L
+
+data ZipException = FileNotFound Text
+    deriving (Show, Typeable)
+instance Exception ZipException
+
+zipScheme :: Monad m
+          => Text -- ^ scheme name
+          -> Archive
+          -> Scheme m
+zipScheme name archive = Scheme
+    { schemeNames = Set.singleton $ snoc name ':'
+    , schemeReader = Just $ \uri step ->
+        case findEntryByPath (unpack $ uriPath uri) archive of
+            Nothing -> throwError $ FileNotFound $ uriPath uri
+            Just e -> enumList 8 (L.toChunks $ fromEntry e) step
+    , schemeWriter = Nothing
+    }
+
+readZipFile :: FilePath -> IO Archive
+readZipFile = fmap toArchive . L.readFile
