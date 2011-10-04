f685a3f763062d3f58679c79f72e31fbea5d018f
Michael Snoyman <michael@snoyman.com>
2011-10-04 07:20:05 +0200

    xml-catalog: nextCatalog, some funny business with dtd-flatten

--- xml-catalog/Text/XML/Catalog.hs
+++ xml-catalog/Text/XML/Catalog.hs
@@ -32,15 +32,16 @@ import Network.URI.Enumerator
 import Network.URI.Enumerator.File (toFilePath)
 import Filesystem.Path.CurrentOS (encodeString)
 import Data.Enumerator (run_, ($$))
-import Data.Enumerator.List (consume)
+--import Data.Enumerator.List (consume)
 import Data.Text.Lazy.Encoding (decodeUtf8With)
 import Data.Text.Encoding.Error (lenientDecode)
-import qualified Data.ByteString.Lazy as L
+--import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import qualified Data.Text as T
-import System.Process (readProcess)
-import Control.Monad (liftM)
+--import System.Process (readProcess)
+--import Control.Monad (liftM)
+import Data.Attoparsec.Text.Enumerator (iterParser)
 
 -- | Either a public or system identifier.
 data PubSys = Public Text | System Text
@@ -56,7 +57,8 @@ loadCatalog sm uri = do
     foldM addNode Map.empty ns
   where
     addNode c (X.NodeElement (X.Element name as ns)) = do
-        foldM addNode c' ns
+        c'' <- c'
+        foldM addNode c'' ns
       where
         -- FIXME handle hierarchies
         base =
@@ -64,27 +66,37 @@ loadCatalog sm uri = do
                 Nothing -> ""
                 Just x -> x
         withBase = T.append base
+
         c' =
             case name of
                 "{urn:oasis:names:tc:entity:xmlns:xml:catalog}public" ->
                     case (lookup "publicId" as, lookup "uri" as) of
                         (Just pid, Just ref) ->
                             case parseURIReference (withBase ref) >>= flip relativeTo uri of
-                                Just uri' -> Map.insert (Public pid) uri' c
-                                Nothing -> c
-                        _ -> c
+                                Just uri' -> return $ Map.insert (Public pid) uri' c
+                                Nothing -> return c
+                        _ -> return c
                 "{urn:oasis:names:tc:entity:xmlns:xml:catalog}system" ->
                     case (lookup "systemId" as, lookup "uri" as) of
                         (Just sid, Just ref) ->
                             case parseURIReference (withBase ref) >>= flip relativeTo uri of
-                                Just uri' -> Map.insert (System sid) uri' c
-                                Nothing -> c
-                        _ -> c
-                _ -> c
+                                Just uri' -> return $ Map.insert (System sid) uri' c
+                                Nothing -> return c
+                        _ -> return c
+                "{urn:oasis:names:tc:entity:xmlns:xml:catalog}nextCatalog" ->
+                    case lookup "catalog" as of
+                        Just catalog ->
+                            case parseURIReference catalog >>= flip relativeTo uri of
+                                Just uri' -> do
+                                    c'' <- loadCatalog sm uri'
+                                    return $ c'' `Map.union` c
+                                Nothing -> return c
+                        Nothing -> return c
+                _ -> return c
     addNode c _ = return c
 
 data DTDCache m = DTDCache
-    { _dcCache :: I.IORef (Map.Map PubSys D.DTD)
+    { _dcCache :: I.IORef (Map.Map PubSys AttrMap)
     , _dcCatalog :: Catalog
     , dcSchemeMap :: SchemeMap m
     , _dcFlattenJar :: String
@@ -95,21 +107,20 @@ newDTDCache fj c sm = do
     x <- liftIO $ I.newIORef Map.empty
     return $ DTDCache x c sm fj
 
-loadDTD :: MonadIO m => DTDCache m -> X.ExternalID -> m D.DTD
-loadDTD (DTDCache icache catalog sm flattenJar) ext = do
+loadDTD :: MonadIO m => DTDCache m -> X.ExternalID -> m AttrMap
+loadDTD (DTDCache icache catalog sm _flattenJar) ext = do
     res <- liftIO $ fmap (Map.lookup pubsys) $ I.readIORef icache
     case res of
         Just dtd -> return dtd
         Nothing ->
             case Map.lookup pubsys catalog of
                 Nothing -> liftIO $ throwIO $ UnknownExternalID ext
-                Just fp -> do
-                    let dtdFP = encodeString $ toFilePath fp
-                    lbs <- liftM L8.pack $ liftIO $ readProcess "java" ["-jar", flattenJar, dtdFP] ""
-                    let dtd = parseDTD $ decodeUtf8With lenientDecode lbs
+                Just uri -> do
+                    attLists <- liftIO $ run_ $ readURI sm uri $$ iterAttLists
+                    let attrMap = toAttrs attLists
                     liftIO $ I.atomicModifyIORef icache $ \m ->
-                        (Map.insert pubsys dtd m, ())
-                    return dtd
+                        (Map.insert pubsys attrMap m, ())
+                    return attrMap
   where
     pubsys =
         case ext of
@@ -125,8 +136,7 @@ applyDTD :: MonadIO m
 applyDTD dc doc@(X.Document pro@(X.Prologue _ mdoctype _) root epi) =
     case mdoctype of
         Just (X.Doctype _ (Just extid)) -> do
-            dtd <- loadDTD dc extid
-            let attrs = toAttrs dtd
+            attrs <- loadDTD dc extid
             let root' = go attrs root
             return $ X.Document pro root' epi
         _ -> return doc
@@ -149,7 +159,9 @@ applyDTD dc doc@(X.Document pro@(X.Prologue _ mdoctype _) root epi) =
 
 data Att = Def Text | Fixed Text
 
-toAttrs :: D.DTD -> Map.Map X.Name [(X.Name, Att)]
+type AttrMap = Map.Map X.Name [(X.Name, Att)]
+
+toAttrs :: D.DTD -> AttrMap
 toAttrs (D.DTD _ comps) =
     Map.fromList $ mapMaybe go comps
   where
@@ -162,3 +174,5 @@ toAttrs (D.DTD _ comps) =
             _ -> Nothing
       where
         name = X.Name lname Nothing Nothing
+
+iterAttLists :: Iteratee Text m 
