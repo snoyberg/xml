841e14a8cf9e587f92fd0c85346fb0909a8802ba
Michael Snoyman <michael@snoyman.com>
2011-10-21 11:14:13 +0200

    DTD cache code resolves entities

--- dtd/Data/DTD/Cache.hs
+++ dtd/Data/DTD/Cache.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Data.DTD.Cache
     ( DTDCache
     , applyDTD
+    , applyDTD_
     , newDTDCache
     , newDTDCacheFile
     , loadAttrMap
+    , UnresolvedEntity (..)
     ) where
 
 import qualified Text.XML as X
+import qualified Data.XML.Types as XU
 import qualified Data.Map as Map
 import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 import Text.XML.Catalog
 import Network.URI.Enumerator
 import Network.URI.Enumerator.File
@@ -26,12 +32,15 @@ import Data.Maybe (mapMaybe)
 import Control.Monad.IO.Control (MonadControlIO)
 import qualified Network.URI as NU
 
-toAttrs :: [D.DTDComponent] -> AttrMap
-toAttrs comps =
-    Map.fromList $ mapMaybe go comps
+toMaps :: [D.DTDComponent] -> (EntityMap, AttrMap)
+toMaps =
+    foldr go (Map.empty, Map.empty)
   where
-    go (D.DTDAttList (D.AttList lname atts)) = Just $ (X.Name lname Nothing Nothing, mapMaybe go' atts)
-    go _ = Nothing
+    go (D.DTDEntityDecl (D.InternalGeneralEntityDecl k v)) (e, a) = (Map.insert k v e, a)
+    go (D.DTDAttList (D.AttList lname atts)) (e, a) =
+        (e, Map.insert (X.Name lname Nothing Nothing) (mapMaybe go' atts) a)
+    go _ m = m
+
     go' (D.AttDecl lname _ def) =
         case def of
             D.AttFixed t -> Just (name, Fixed t)
@@ -41,7 +50,7 @@ toAttrs comps =
         name = X.Name lname Nothing Nothing
 
 data DTDCache = DTDCache
-    { _dcCache :: I.IORef (Map.Map PubSys AttrMap)
+    { _dcCache :: I.IORef (Map.Map PubSys (EntityMap, AttrMap))
     , _dcCatalog :: Catalog
     , _dcSchemeMap :: forall m. MonadControlIO m => SchemeMap m
     }
@@ -57,7 +66,7 @@ newDTDCacheFile fp = do
     c <- loadCatalog (toSchemeMap [fileScheme]) uri
     newDTDCache c (toSchemeMap [fileScheme])
 
-loadAttrMap :: MonadControlIO m => DTDCache -> X.ExternalID -> m AttrMap
+loadAttrMap :: MonadControlIO m => DTDCache -> X.ExternalID -> m (EntityMap, AttrMap)
 loadAttrMap (DTDCache icache catalog sm) ext = do
     res <- liftIO $ fmap (Map.lookup pubsys) $ I.readIORef icache
     case res of
@@ -68,10 +77,10 @@ loadAttrMap (DTDCache icache catalog sm) ext = do
                 Just uri -> do
                     ecomps <- E.run $ readEID catalog (uriToEID uri) sm E.$$ EL.consume
                     comps <- either (liftIO . throwIO . CannotLoadDTD (toNetworkURI uri)) return ecomps
-                    let attrMap = toAttrs comps
+                    let maps = toMaps comps
                     liftIO $ I.atomicModifyIORef icache $ \m ->
-                        (Map.insert pubsys attrMap m, ())
-                    return attrMap
+                        (Map.insert pubsys maps m, ())
+                    return maps
   where
     pubsys =
         case ext of
@@ -83,32 +92,68 @@ data UnknownExternalID = UnknownExternalID X.ExternalID
     deriving (Show, Typeable)
 instance Exception UnknownExternalID
 
+data UnresolvedEntity = UnresolvedEntity Text
+    deriving (Show, Typeable)
+instance Exception UnresolvedEntity
+
+applyDTD_ :: MonadControlIO m
+          => DTDCache -> XU.Document -> m X.Document
+applyDTD_ dc doc = applyDTD dc doc >>= either (liftIO . throwIO) return
+
 applyDTD :: MonadControlIO m
-         => DTDCache -> X.Document -> m X.Document
-applyDTD dc doc@(X.Document pro@(X.Prologue _ mdoctype _) root epi) =
+         => DTDCache -> XU.Document -> m (Either UnresolvedEntity X.Document)
+applyDTD dc doc@(XU.Document pro@(X.Prologue _ mdoctype _) root epi) =
     case mdoctype of
         Just (X.Doctype _ (Just extid)) -> do
             attrs <- loadAttrMap dc extid
-            let root' = go attrs root
-            return $ X.Document pro root' epi
-        _ -> return doc
+            case go attrs root of
+                Left e -> return $ Left e
+                Right root' -> return $ Right $ X.Document pro root' epi
+        _ -> return $ goD (Map.empty, Map.empty) doc
   where
-    go attrs (X.Element name as ns) =
-        X.Element name as' ns'
+    go :: (EntityMap, AttrMap) -> XU.Element -> Either UnresolvedEntity X.Element
+    go (ents, attrs) (XU.Element name as ns) = do
+        as' <- mapM (resolveAttr ents) as
+        ns' <- mapM gon ns
+        Right $ X.Element name (as'' as') ns'
       where
-        as' =
+        as'' as' =
             case Map.lookup name attrs of
-                Nothing -> as
-                Just x -> foldr goa as x
-        ns' = map gon ns
-        gon (X.NodeElement e) = X.NodeElement $ go attrs e
-        gon n = n
+                Nothing -> as'
+                Just x -> foldr goa as' x
+        gon (XU.NodeElement e) = fmap X.NodeElement $ go (ents, attrs) e
+        gon (XU.NodeComment t) = Right $ X.NodeComment t
+        gon (XU.NodeInstruction t) = Right $ X.NodeInstruction t
+        gon (XU.NodeContent (XU.ContentText t)) = Right $ X.NodeContent t
+        gon (XU.NodeContent (XU.ContentEntity t)) = fmap X.NodeContent $ getEntity ents t
+
     goa (name, Fixed t) as = (name, t) : filter (\(n, _) -> name /= n) as
     goa (name, Def t) as =
         case lookup name as of
             Nothing -> (name, t) : as
             Just _ -> as
 
+    goD attrs (XU.Document a r b) =
+        case go attrs r of
+            Left e -> Left e
+            Right root' -> Right $ X.Document a root' b
+
+    resolveAttr ents (k, v) = fmap (\ts -> (k, T.concat ts)) $ mapM (resolveAttr' ents) v
+    resolveAttr' _ (XU.ContentText t) = Right t
+    resolveAttr' ents (XU.ContentEntity t) = getEntity ents t
+
 data Att = Def Text | Fixed Text
 
 type AttrMap = Map.Map X.Name [(X.Name, Att)]
+type EntityMap = Map.Map Text Text
+
+getEntity :: EntityMap -> Text -> Either UnresolvedEntity Text
+getEntity ents t = maybe (Left $ UnresolvedEntity t) Right $ do
+    raw <- Map.lookup t ents
+    case X.parseText X.def $ TL.fromChunks ["<dummy>", raw, "</dummy>"] of
+        Right (X.Document _ (X.Element _ _ nodes) _) -> toContent nodes
+        Left{} -> Nothing
+  where
+    toContent = fmap T.concat . mapM toContent'
+    toContent' (X.NodeContent t) = Just t
+    toContent' _ = Nothing
--- dtd/test.hs
+++ dtd/test.hs
@@ -2,11 +2,15 @@
 import System.Environment (getArgs)
 import Data.DTD.Cache
 import qualified Text.XML as X
+import qualified Text.XML.Unresolved as XU
 import qualified Data.ByteString.Lazy as L
+import Control.Monad
 
 main :: IO ()
 main = do
-    [catfile, file] <- getArgs
-    doc <- X.readFile_ X.def file
+    (catfile:files) <- getArgs
     cache <- newDTDCacheFile catfile
-    applyDTD cache doc >>= L.putStr . X.renderLBS X.def
+    forM_ files $ \file -> do
+        doc <- XU.readFile_ X.def file
+        let file' = file ++ ".applied"
+        applyDTD_ cache doc >>= X.writeFile X.def file'
