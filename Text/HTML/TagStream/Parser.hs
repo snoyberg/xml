770ff3489499c51bd3f052e7688800925f5b2da4
yihuang <yi.codeplayer@gmail.com>
2011-11-02 16:57:23 +0800

    parse double quote in attribute.

--- Text/HTML/TagStream/Parser.hs
+++ Text/HTML/TagStream/Parser.hs
@@ -3,10 +3,18 @@ module Text.HTML.TagStream.Parser where
 
 import Control.Applicative ( (<$>), (<*), (<|>) )
 import Data.Attoparsec.Char8
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as S
 import Text.HTML.TagStream.Types
 
--- TODO handle double-quote
+-- TODO handle escape double-quote
+value :: Parser ByteString
+value = do
+    c <- anyChar
+    case c of
+        '"' -> takeTill (=='"') <* anyChar
+        _   -> S.cons c <$> takeTill (inClass ">= ")
+
 attr :: Parser Attr
 attr = do
     skipSpace
@@ -17,7 +25,8 @@ attr = do
     option (name, S.empty) $ do
         _ <- char '='
         skipSpace
-        (name,) <$> takeTill (inClass ">= ")
+        (name,) <$> value
+        -- (name,) <$> takeTill (inClass ">= ")
 
 attrs :: Parser [Attr]
 attrs = many attr
