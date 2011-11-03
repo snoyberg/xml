62b0845c2c61dc30526612cb820a02f3334b65d7
yihuang <yi.codeplayer@gmail.com>
2011-11-03 17:45:51 +0800

    new demonstration program.

--- FilterUrl.hs
+++ FilterUrl.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Control.Applicative
 import System.Environment (getArgs)
 import System.IO (openBinaryFile, IOMode(..))
 import qualified Data.Enumerator.Binary as E
@@ -6,20 +7,36 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as S
 import qualified Data.Enumerator as E
 import qualified Data.Enumerator.List as EL
+import Data.Attoparsec.Char8
 import Text.HTML.TagStream
+
+type Protocol = ByteString
+type Domain = ByteString
+type Path = ByteString
+url :: Parser (Protocol, Domain, Path)
+url = (,,) <$> (string "http://" <|> string "https://")
+           <*> takeTill (=='/')
+           <*> takeByteString
+
+changeUrl :: ByteString -> ByteString
+changeUrl s = either (const s) changeDomain $ parseOnly url s
+  where
+    changeDomain (prop, domain, path) =
+      S.concat [ prop
+               , domain
+               , ".proxy.com"
+               , path
+               ]
 
 withUrl :: Monad m => (ByteString -> ByteString) -> E.Enumeratee Token Token m b
 withUrl f = EL.map filter'
   where filter' :: Token -> Token
-        filter' (TagOpen name as)  = TagOpen name (map filter'' as)
+        filter' (TagOpen name as close)  = TagOpen name (map filter'' as) close
         filter' t = t
         filter'' :: Attr -> Attr
         filter'' (name, value)
             | name=="href" || name=="src" = (name, f value)
             | otherwise = (name, value)
-
-changeHost :: ByteString -> ByteString
-changeHost = id
 
 main :: IO ()
 main = do
@@ -27,5 +44,5 @@ main = do
     h <- openBinaryFile filename ReadMode
     let bufSize = 2
         enum = E.enumHandle bufSize h
-    tokens <- E.run_ $ ((enum E.$= tokenStream) E.$= withUrl changeHost) E.$$ EL.consume
-    S.putStrLn $ S.concat $ map showToken tokens
+    tokens <- E.run_ $ ((enum E.$= tokenStream) E.$= withUrl changeUrl) E.$$ EL.consume
+    S.putStrLn $ S.concat $ map (showToken id) tokens
