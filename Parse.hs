c6e4d308791dcd184df33d7468a9bd756e26cc8d
yihuang <yi.codeplayer@gmail.com>
2011-11-04 16:57:11 +0800

    parse benchmark program

---
+++ Parse.hs
@@ -0,0 +1,17 @@
+import Data.Maybe
+import System.IO
+import System.Environment
+import Text.HTML.TagStream
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as S
+import qualified Data.Enumerator as E
+import qualified Data.Enumerator.Binary as E
+import qualified Data.Enumerator.List as EL
+
+main :: IO ()
+main = do
+    args <- getArgs
+    filename <- maybe (fail "pass file path") return (listToMaybe args)
+    let enum = E.enumFile filename E.$= tokenStream
+    E.run_ $ enum E.$$ EL.consume
+    return ()
