70e78787b6d259e3040ea90a2a4ea4baa55fed7c
Erik de Castro Lopo <erikd@mega-nerd.com>
2012-02-14 14:34:40 +1100

    xml-conduit : Make example code and haddock example compile.

--- xml-conduit/parser-sample.hs
+++ xml-conduit/parser-sample.hs
@@ -1,14 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Text.XML.Enumerator.Parse
-import Data.Text.Lazy (Text, unpack)
+import Control.Monad.Trans.Resource
+import Data.Conduit (Sink, ($$))
+import Data.Text (Text, unpack)
+import Data.XML.Types (Event)
+import Text.XML.Stream.Parse
 
-data Person = Person { age :: Int, name :: Text }
+data Person = Person Int Text
     deriving Show
 
-parsePerson = tag' "person" (requireAttr "age") $ \age -> do
-    name <- content'
+parsePerson :: ResourceThrow m => Sink Event m (Maybe Person)
+parsePerson = tagName "person" (requireAttr "age") $ \age -> do
+    name <- content
     return $ Person (read $ unpack age) name
 
-parsePeople = tag'' "people" $ many parsePerson
+parsePeople :: Sink Event IO (Maybe [Person])
+parsePeople = tagNoAttr "people" $ many parsePerson
 
-main = parseFile_ "people.xml" (const Nothing) $ force "people required" parsePeople
+main :: IO ()
+main = do
+    people <- runResourceT $
+            parseFile def "people.xml" $$ force "people required" parsePeople
+    print people
+
