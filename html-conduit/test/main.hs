4ed06f7a35a903581c8c1ab792d9efb36de260b1
Michael Snoyman <michael@snoyman.com>
2012-06-26 09:16:13 +0300

    Added more html tests

--- html-conduit/test/main.hs
+++ html-conduit/test/main.hs
@@ -5,6 +5,7 @@ import Test.Hspec.HUnit ()
 import Data.ByteString.Lazy.Char8 ()
 import qualified Text.HTML.DOM as H
 import qualified Text.XML as X
+import qualified Data.Map as Map
 
 main :: IO ()
 main = hspec $ do
@@ -33,3 +34,33 @@ main = hspec $ do
         it "invalid entities" $
             X.parseLBS_ X.def "<foo><bar>baz&amp;foobar;</bar></foo>" @=?
             H.parseLBS        "<foo><bar>baz&foobar;</foo>"
+    describe "HTML parsing" $ do
+        it "XHTML" $
+            let html = "<html><head><title>foo</title></head><body><p>Hello World</p></body></html>"
+                doc = X.Document (X.Prologue [] Nothing []) root []
+                root = X.Element "html" Map.empty
+                    [ X.NodeElement $ X.Element "head" Map.empty
+                        [ X.NodeElement $ X.Element "title" Map.empty
+                            [X.NodeContent "foo"]
+                        ]
+                    , X.NodeElement $ X.Element "body" Map.empty
+                        [ X.NodeElement $ X.Element "p" Map.empty
+                            [X.NodeContent "Hello World"]
+                        ]
+                    ]
+             in H.parseLBS html @?= doc
+        it "HTML" $
+            let html = "<html><head><title>foo</title></head><body><br><p>Hello World</p></body></html>"
+                doc = X.Document (X.Prologue [] Nothing []) root []
+                root = X.Element "html" Map.empty
+                    [ X.NodeElement $ X.Element "head" Map.empty
+                        [ X.NodeElement $ X.Element "title" Map.empty
+                            [X.NodeContent "foo"]
+                        ]
+                    , X.NodeElement $ X.Element "body" Map.empty
+                        [ X.NodeElement $ X.Element "br" Map.empty []
+                        , X.NodeElement $ X.Element "p" Map.empty
+                            [X.NodeContent "Hello World"]
+                        ]
+                    ]
+             in H.parseLBS html @?= doc
