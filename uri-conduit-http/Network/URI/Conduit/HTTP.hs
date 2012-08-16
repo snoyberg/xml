f153867873a51fae2688ddac740d8072c6357c6d
Michael <michael@snoyman.com>
2012-08-16 14:47:06 +0300

    uri-conduit-http (not done)

--- uri-conduit-http/Network/URI/Conduit/HTTP.hs
+++ uri-conduit-http/Network/URI/Conduit/HTTP.hs
@@ -1,34 +1,35 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
-module Network.URI.Enumerator.HTTP
+module Network.URI.Conduit.HTTP
     ( httpScheme
     , withManager
     ) where
 
-import Network.URI.Enumerator (Scheme (..), toNetworkURI)
-import Network.HTTP.Enumerator (Manager, parseUrl, httpRedirect, withManager, HttpException)
+import Network.URI.Conduit (Scheme (..), toNetworkURI, URI)
+import Data.ByteString (ByteString)
+import Network.HTTP.Conduit
 import Network.HTTP.Types (Status, status200)
-import Data.Enumerator (throwError, returnI)
+import Data.Conduit
+import Data.Conduit.Internal (sourceToPipe)
 import Control.Exception (Exception)
 import Data.Typeable (Typeable)
 import qualified Data.Set as Set
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.IO.Class (liftIO)
-import Control.Failure (Failure)
-
-data HTTPURIException = NotOKResponse Status
-    deriving (Show, Typeable)
-instance Exception HTTPURIException
+import Control.Monad.Trans.Resource (register)
 
 httpScheme :: Manager -> Scheme
 httpScheme m = Scheme
     { schemeNames = Set.fromList ["http:", "https:"]
-    , schemeReader = Just $ \uri step -> do
-        req <- liftIO $ parseUrl $ show $ toNetworkURI uri
-        flip (httpRedirect req) m $ \status _ ->
-            if status == status200
-                then returnI step
-                else throwError $ NotOKResponse status
+    , schemeReader = Just (httpReader m)
     , schemeWriter = Nothing
     }
+
+--httpReader :: Manager -> URI -> Pipe l i ByteString u (ResourceT IO) ()
+httpReader m uri = do
+    req <- liftIO $ parseUrl $ show $ toNetworkURI uri
+    res <- lift $ http req m
+    (src, final) <- lift $ unwrapResumable $ responseBody res
+    sourceToPipe src
+    lift final
