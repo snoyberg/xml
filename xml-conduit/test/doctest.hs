96b0d22d29e958e803645fca36e0de62a0fc3afe
koral <chahine.moreau@gmail.com>
2020-02-02 08:34:47 +0100

    Fix doctests

--- xml-conduit/test/doctest.hs
+++ xml-conduit/test/doctest.hs
@@ -1,3 +1,13 @@
-import           Test.DocTest
+module Main where
 
-main = doctest ["Text"]
+import           Build_doctests     (flags, module_sources, pkgs)
+import           Data.Foldable      (traverse_)
+import           System.Environment (unsetEnv)
+import           Test.DocTest       (doctest)
+
+main :: IO ()
+main = do
+    traverse_ putStrLn args
+    doctest args
+  where
+    args = flags ++ pkgs ++ module_sources
