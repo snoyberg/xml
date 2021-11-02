582bf7c90700319d4cf95ae9a5a5fe2c2fcec5e9
koral <mail@cmoreau.info>
2021-11-02 21:10:00 +0100

    Disable doctest to support GHC 9.2

--- xml-conduit/Setup.hs
+++ xml-conduit/Setup.hs
@@ -1,6 +1,2 @@
-module Main where
-
-import Distribution.Extra.Doctest (defaultMainWithDoctests)
-
-main :: IO ()
-main = defaultMainWithDoctests "doctest"
+import Distribution.Simple
+main = defaultMain
